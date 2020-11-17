be2e1cb0b7fa4fda062e8ed9df006ad3caea7427
Scott Kulp <skulp@climatecentral.org>
2020-11-17 18:26:21 -0500

    Removed irrelevant scripts

--- PointsInShp.m
+++
@@ -1,9 +0,0 @@
-function [result] = PointsInShp(filename, points, placeName)
-
-    shp = shaperead(filename,'Selector',{@(v1)(strcmp(v1,placeName)),'NAME10'});
-    
-    
-    
-    result = inpolygon(points(:,1),points(:,2), shp.X, shp.Y);
-
-end
